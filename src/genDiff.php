18bb66f0eb0a9a67e174250a4b78a5347d5fb6e3
Ruslan Demin <laikernest@gmail.com>
2022-01-02 17:29:45 +0700

    refactoring and step 8

--- src/genDiff.php
+++ src/genDiff.php
@@ -2,51 +2,207 @@
 
 namespace Gendiff\Gendiff;
 
-function genDiff(string $pathToFile1, string $pathToFile2, string $format) :string
+use stdClass;
+use Symfony\Component\Yaml\Yaml;
+
+function genDiff(string $pathToFile1, string $pathToFile2, string $format = ''): string
 {
-    $dataFirst = \file_get_contents($pathToFile1);
-    $dataSecond = \file_get_contents($pathToFile2);
+    $objectFirst = getArrayEntity($pathToFile1);
+    $objectSecond = getArrayEntity($pathToFile2);
+    $arDiff = diff($objectFirst, $objectSecond);
+
     if ($format == 'json') {
-        return diffJson($dataFirst, $dataSecond);
+        return json_encode($arDiff);
+    }
+
+    if ($format == 'plain') {
+        return formatPlain($arDiff);
+    }
+
+    return formatDefault($arDiff);
+}
+
+function getArrayEntity($pathToFile)
+{
+    $extension = pathinfo($pathToFile, PATHINFO_EXTENSION);
+    $fileRawData = \file_get_contents($pathToFile);
+    if ($extension == 'json') {
+        return \json_decode($fileRawData);
+    }
+
+    if ($extension == 'yml' || $extension == 'yaml') {
+        return Yaml::parse($fileRawData, Yaml::PARSE_OBJECT_FOR_MAP);
     }
 }
 
-function diffJson($jsonFirst, $jsonSecond)
+function diff($objectFirst, $objectSecond)
 {
-    $arDiff = [];
-    $diffString = '{' . PHP_EOL;
-    $arJsonFirst = \json_decode($jsonFirst, true);
-    $arJsonSecond = \json_decode($jsonSecond, true);
-    foreach ($arJsonFirst as $key => $value) {
-        $value = sanitizeValue($value);
-        if (!array_key_exists($key, $arJsonSecond)) {
-            $arDiff[$key . '_a'] = "  - {$key}: {$value}";
-        } else {
-            if ($value != sanitizeValue($arJsonSecond[$key])) {
-                $arDiff[$key . '_a'] = "  - {$key}: {$value}";
-                $arDiff[$key . '_b'] = "  + {$key}: {$arJsonSecond[$key]}";
+
+    $iter = function ($objectFirst, $objectSecond) use (&$iter) {
+
+        $arDiff = [];
+
+        foreach ($objectFirst as $keyFirst => $valueFirst) {
+            if (is_object($valueFirst)) {
+                $arDiff[$keyFirst]['old'] = $iter($valueFirst, $objectSecond->{$keyFirst});
             } else {
-                $arDiff[$key . '_a'] = "    {$key}: {$value}";
+                $arDiff[$keyFirst]['old'] = sanitizeValue($valueFirst);
+            }
+
+            if (\property_exists($objectSecond, $keyFirst)) {
+                if (is_object($objectSecond->{$keyFirst})) {
+                    $arDiff[$keyFirst]['new'] = $iter($valueFirst, $objectSecond->{$keyFirst});
+                } else {
+                    $arDiff[$keyFirst]['new'] = sanitizeValue($objectSecond->{$keyFirst});
+                }
             }
         }
-    }
 
-    foreach ($arJsonSecond as $key => $value) {
-        $value = \var_export($value, true);
-        if (!array_key_exists($key, $arJsonFirst)) {
-            $arDiff[$key . '_b'] = "  + {$key}: {$value}";
+        foreach ($objectSecond as $keySecond => $valueSecond) {
+            if (!\property_exists($objectFirst, $keySecond)) {
+                if (is_object($valueSecond)) {
+                    $arDiff[$keySecond]['new'] = $iter($objectSecond->{$keySecond}, $valueSecond);
+                } else {
+                    $arDiff[$keySecond]['new'] = sanitizeValue($valueSecond);
+                }
+            }
         }
-    }
 
-    ksort($arDiff);
+        ksort($arDiff);
 
-    $diffString .= implode(PHP_EOL, $arDiff);
-    $diffString .= PHP_EOL . '}';
+        return $arDiff;
+    };
 
-    return $diffString;
+    $arData = $iter($objectFirst, $objectSecond);
+
+    return $arData;
+}
+
+function formatDefault($arDiff)
+{
+    $iter = function ($arDiff, $level, $diffParent = false) use (&$iter) {
+
+        $diffString = '{' . PHP_EOL;
+
+        $arFormatDiff = [];
+
+        foreach ($arDiff as $key => $value) {
+            $noDiffValue = false;
+
+            if (\is_array($value['old']) && !\is_array($value['new'])) {
+                $noDiffValue = true;
+            }
+            $valueOld = \is_array($value['old']) ? $iter($value['old'], $level + 2, $noDiffValue) : $value['old'];
+            $valueNew = \is_array($value['new']) ? $iter($value['new'], $level + 2) : $value['new'];
+
+            if ($diffParent) {
+                $diff = ' ';
+            } else {
+                $diff = '-';
+            }
+
+            if (\array_key_exists('old', $value) && \array_key_exists('new', $value)) {
+                if ($valueOld != $valueNew) {
+                    $arFormatDiff[] = str_repeat('  ', $level) .  $diff . ' ' . $key . ': ' . $valueOld;
+                    $arFormatDiff[] = str_repeat('  ', $level) .  '+' . ' ' . $key . ': ' . $valueNew;
+                } else {
+                    $arFormatDiff[] = str_repeat('  ', $level) .  ' ' . ' ' . $key . ': ' . $valueNew;
+                }
+            } else {
+                if ($value['old']) {
+                    $arFormatDiff[] = str_repeat('  ', $level) .  $diff . ' ' . $key . ': ' . $valueOld;
+                } else {
+                    $arFormatDiff[] = str_repeat('  ', $level) .  '+' . ' ' . $key . ': ' . $valueNew;
+                }
+            }
+        }
+
+        $diffString .= implode(PHP_EOL, $arFormatDiff);
+
+        if ($level > 1) {
+            $diffString .= PHP_EOL . str_repeat('  ', $level - 1)  . '}';
+        } else {
+            $diffString .= PHP_EOL . '}';
+        }
+
+
+        return $diffString;
+    };
+
+    return $iter($arDiff, 1, false);
+}
+
+function formatPlain($arDiff)
+{
+
+    $iter = function ($arDiff, &$arFormatDiff = [], $level = 1, $currentLevel = '') use (&$iter) {
+
+        foreach ($arDiff as $key => $value) {
+            $noDiffValue = (\is_array($value['old']) && !\is_array($value['new']));
+            $tempLevel = ($level == 1) ? $key : $currentLevel . '.' . $key;
+
+            if (\is_array($value['old']) && !$noDiffValue) {
+                $valueOld = $iter($value['old'], $arFormatDiff, $level + 1, $tempLevel);
+            } else {
+                $valueOld = sanitizeValuePlain($value['old']);
+            }
+
+            if (\is_array($value['new']) && array_key_exists('old', $value)) {
+                $valueNew = $iter($value['new'], $arFormatDiff, $level + 1, $tempLevel);
+            } else {
+                $valueNew = sanitizeValuePlain($value['new']);
+            }
+
+            $formatString = '';
+            if (\array_key_exists('old', $value) && \array_key_exists('new', $value)) {
+                if ($valueOld != $valueNew) {
+                    $formatString = "Property '" . $tempLevel . "' was updated. From " . $valueOld . " to " . $valueNew;
+                }
+            } else {
+                if ($value['old']) {
+                    $formatString = "Property '" . $tempLevel . "' was removed";
+                } else {
+                    $formatString = "Property '" . $tempLevel . "' was added with value: " . $valueNew;
+                }
+            }
+
+            if ($formatString && !in_array($formatString, $arFormatDiff)) {
+                $arFormatDiff[] = $formatString;
+            }
+        }
+
+        return $arFormatDiff;
+    };
+
+    $arFormatDiff = $iter($arDiff);
+    return implode(PHP_EOL, $arFormatDiff);
 }
 
 function sanitizeValue($value)
 {
+    if (is_object($value)) {
+        return (array) $value;
+    }
+
     return \str_replace('"', '', \json_encode($value));
+}
+
+function toString($value)
+{
+    return trim(var_export(\json_encode($value), true), "'");
+}
+
+function sanitizeValuePlain($value)
+{
+    if (is_object($value) || is_array($value)) {
+        return '[complex value]';
+    }
+
+    $value = \str_replace('"', '', $value);
+
+    if (in_array(trim($value), ['true', 'false', 'null'])) {
+        return $value;
+    }
+
+    return "'{$value}'";
 }
--- tests/src/genDiffTest.php
+++ tests/src/genDiffTest.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace Gendiff\Gendiff\Tests;
 
 use PHPUnit\Framework\TestCase;
+
 use function Gendiff\Gendiff\genDiff;
 
-class genDiffTest extends TestCase
+class GenDiffTest extends TestCase
 {
     public function testJsonDiff()
     {
@@ -19,6 +21,180 @@ class genDiffTest extends TestCase
           + timeout: 20
           + verbose: true
         }
+        DOC;
+        $this->assertSame($resultString, genDiff($beforePath, $afterPath));
+    }
+
+    public function testYamlDiff()
+    {
+        $beforePath = 'tests/fixtures/before.yml';
+        $afterPath = 'tests/fixtures/after.yml';
+        $resultString = <<<DOC
+        {
+          - follow: false
+            host: hexlet.io
+          - proxy: 123.234.53.22
+          - timeout: 50
+          + timeout: 20
+          + verbose: true
+        }
+        DOC;
+        $this->assertSame($resultString, genDiff($beforePath, $afterPath));
+    }
+
+    public function testDifferentFormatsDiff()
+    {
+        $beforePath = 'tests/fixtures/before.json';
+        $afterPath = 'tests/fixtures/after.yaml';
+        $resultString = <<<DOC
+        {
+          - follow: false
+            host: hexlet.io
+          - proxy: 123.234.53.22
+          - timeout: 50
+          + timeout: 20
+          + verbose: true
+        }
+        DOC;
+        $this->assertSame($resultString, genDiff($beforePath, $afterPath));
+    }
+
+    public function testRecursiveDiff()
+    {
+
+        $beforePath = 'tests/fixtures/beforeRec.json';
+        $afterPath = 'tests/fixtures/afterRec.json';
+        $resultString = <<<DOC
+        {
+            common: {
+              + follow: false
+                setting1: Value 1
+              - setting2: 200
+              - setting3: true
+              + setting3: null
+              + setting4: blah blah
+              + setting5: {
+                    key5: value5
+                }
+                setting6: {
+                    doge: {
+                      - wow: 
+                      + wow: so much
+                    }
+                    key: value
+                  + ops: vops
+                }
+            }
+            group1: {
+              - baz: bas
+              + baz: bars
+                foo: bar
+              - nest: {
+                    key: value
+                }
+              + nest: str
+            }
+          - group2: {
+                abc: 12345
+                deep: {
+                    id: 45
+                }
+            }
+          + group3: {
+                deep: {
+                    id: {
+                        number: 45
+                    }
+                }
+                fee: 100500
+            }
+        }
+        DOC;
+        $this->assertSame($resultString, genDiff($beforePath, $afterPath));
+    }
+
+    public function testRecursiveDiffYaml()
+    {
+
+        $beforePath = 'tests/fixtures/beforeRec.yaml';
+        $afterPath = 'tests/fixtures/afterRec.yaml';
+        $resultString = <<<DOC
+        {
+            common: {
+              + follow: false
+                setting1: Value 1
+              - setting2: 200
+              - setting3: true
+              + setting3: null
+              + setting4: blah blah
+              + setting5: {
+                    key5: value5
+                }
+                setting6: {
+                    doge: {
+                      - wow: 
+                      + wow: so much
+                    }
+                    key: value
+                  + ops: vops
+                }
+            }
+            group1: {
+              - baz: bas
+              + baz: bars
+                foo: bar
+              - nest: {
+                    key: value
+                }
+              + nest: str
+            }
+          - group2: {
+                abc: 12345
+                deep: {
+                    id: 45
+                }
+            }
+          + group3: {
+                deep: {
+                    id: {
+                        number: 45
+                    }
+                }
+                fee: 100500
+            }
+        }
+        DOC;
+        $this->assertSame($resultString, genDiff($beforePath, $afterPath));
+    }
+
+    public function testRecursiveDiffPlain()
+    {
+
+        $beforePath = 'tests/fixtures/beforeRec.json';
+        $afterPath = 'tests/fixtures/afterRec.json';
+        $resultString = <<<DOC
+        Property 'common.follow' was added with value: false
+        Property 'common.setting2' was removed
+        Property 'common.setting3' was updated. From true to null
+        Property 'common.setting4' was added with value: 'blah blah'
+        Property 'common.setting5' was added with value: [complex value]
+        Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+        Property 'common.setting6.ops' was added with value: 'vops'
+        Property 'group1.baz' was updated. From 'bas' to 'bars'
+        Property 'group1.nest' was updated. From [complex value] to 'str'
+        Property 'group2' was removed
+        Property 'group3' was added with value: [complex value]
+        DOC;
+        $this->assertSame($resultString, genDiff($beforePath, $afterPath, 'plain'));
+    }
+
+    public function testRecursiveJson()
+    {
+
+        $beforePath = 'tests/fixtures/beforeRec.json';
+        $afterPath = 'tests/fixtures/afterRec.json';
+        $resultString = <<<DOC
+        {"common":{"old":{"follow":{"new":"false"},"setting1":{"old":"Value 1","new":"Value 1"},"setting2":{"old":"200"},"setting3":{"old":"true","new":"null"},"setting4":{"new":"blah blah"},"setting5":{"new":{"key5":{"old":"value5","new":"value5"}}},"setting6":{"old":{"doge":{"old":{"wow":{"old":"","new":"so much"}},"new":{"wow":{"old":"","new":"so much"}}},"key":{"old":"value","new":"value"},"ops":{"new":"vops"}},"new":{"doge":{"old":{"wow":{"old":"","new":"so much"}},"new":{"wow":{"old":"","new":"so much"}}},"key":{"old":"value","new":"value"},"ops":{"new":"vops"}}}},"new":{"follow":{"new":"false"},"setting1":{"old":"Value 1","new":"Value 1"},"setting2":{"old":"200"},"setting3":{"old":"true","new":"null"},"setting4":{"new":"blah blah"},"setting5":{"new":{"key5":{"old":"value5","new":"value5"}}},"setting6":{"old":{"doge":{"old":{"wow":{"old":"","new":"so much"}},"new":{"wow":{"old":"","new":"so much"}}},"key":{"old":"value","new":"value"},"ops":{"new":"vops"}},"new":{"doge":{"old":{"wow":{"old":"","new":"so much"}},"new":{"wow":{"old":"","new":"so much"}}},"key":{"old":"value","new":"value"},"ops":{"new":"vops"}}}}},"group1":{"old":{"baz":{"old":"bas","new":"bars"},"foo":{"old":"bar","new":"bar"},"nest":{"old":{"key":{"old":"value"}},"new":"str"}},"new":{"baz":{"old":"bas","new":"bars"},"foo":{"old":"bar","new":"bar"},"nest":{"old":{"key":{"old":"value"}},"new":"str"}}},"group2":{"old":{"abc":{"old":"12345"},"deep":{"old":{"id":{"old":"45"}}}}},"group3":{"new":{"deep":{"old":{"id":{"old":{"number":{"old":"45","new":"45"}},"new":{"number":{"old":"45","new":"45"}}}},"new":{"id":{"old":{"number":{"old":"45","new":"45"}},"new":{"number":{"old":"45","new":"45"}}}}},"fee":{"old":"100500","new":"100500"}}}}
         DOC;
         $this->assertSame($resultString, genDiff($beforePath, $afterPath, 'json'));
     }
